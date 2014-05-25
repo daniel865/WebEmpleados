12401ff0870148e6d2b3ede73cca20761b2e67e5
Daniel <Daniel@daniel-pc>
2014-05-25 16:14:46 -0500

    new commit

---
+++ src/java/com/webempleados/componentes/Inserciones.sql
@@ -0,0 +1,3 @@
+INSERT INTO CARGOS(codigo_car, descripcion_car) VALUES(1, 'Gerente');
+INSERT INTO CARGOS(codigo_car, descripcion_car) VALUES(2, 'Administrador');
+INSERT INTO CARGOS(codigo_car, descripcion_car) VALUES(3, 'Auxiliar');
